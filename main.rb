a6c8b60808c74f9a43564ffdd486071e4feb02a1
Makotox0207 <xiaoxishicheng@gmail.com>
2021-09-15 20:45:05 +0900

    勇者クラスのパラメータの取得

--- main.rb
+++ main.rb
@@ -1,20 +1,36 @@
 class Brave
 
+  # nameのゲッター
+  def name
+    @name
+  end
   # nameのセッター
   def name=(name)
     @name = name
   end
 
+  # hpのゲッター
+  def hp
+    @hp
+  end
   # hpのセッター
   def hp=(hp)
     @hp = hp
   end
 
+  # offenseのゲッター
+  def offense
+    @offense
+  end
   # offenseのセッター
   def offense=(offense)
     @offense = offense
   end
 
+  # defenseのゲッター
+  def defense
+    @defense
+  end
   # defenseのセッター
   def defense=(defense)
     @defense = defense
@@ -28,3 +44,11 @@ brave.name = "テリー"
 brave.hp = 500
 brave.offense = 150
 brave.defense = 100
+
+# ヒアドキュメント putsや文字列を囲む""（ダブルクウォーテーション）を何度も書く必要がなくなる
+puts <<~TEXT
+NAME:#{brave.name}
+HP:#{brave.hp}
+OFFENSE:#{brave.offense}
+DEFENSE:#{brave.defense}
+TEXT
